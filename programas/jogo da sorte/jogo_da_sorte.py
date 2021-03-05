f5d7ba873483639be120bb58e383411d763d08d8
José Pedro Daniel Dembo <68882941+josedembo@users.noreply.github.com>
2021-03-05 01:57:26 -0300

    actualizando o codigo do jogo da sorte

--- programas/jogo da sorte/jogo_da_sorte.py
+++ programas/jogo da sorte/jogo_da_sorte.py
@@ -4,7 +4,8 @@ cidades = ['Sao Paulo', 'Ceara', 'Recife',
            'Alagoas', 'Rio Grande do Sul', 'Rio de Janeiro']
 numeros = [1, 45, 100, 19, 519, 200]
 tentativas = 0
-while tentativas != 0:
+
+while tentativas < 2:
 
     print('================JOGO DA SORTE======================\n')
     print('seja bem-vido\n')
@@ -15,20 +16,27 @@ while tentativas != 0:
     print('=====(1)Sao Paulo\n=====(2)Ceara\n=====(3)Recife\n=====(4)Alagoas\n=====(5)Rio Grande do Sul\n=====(6)Rio de Janeiro\n')
     print('Os numeros sao:\n===== 1\n===== 45\n===== 100\n===== 19\n===== 519\n===== 200\n')
 
-    nome = input('Informe o seu nome\nR:')
-    tipo_de_jogo = int(input(
-        nome, 'qual o tipo e sorteio que você pretende jogar? \ndigite 1 para "cidade da Sorte" ou 2 para numero da sorte'))
+    if tentativas ==0:
+        nome = input('Informe o seu nome\nR:')
+
+    tipo_de_jogo =int(input('qual o tipo e sorteio que você pretende jogar? \ndigite 1 para cidade da Sorte ou 2 para numero da sorte'))
 
     if tipo_de_jogo == 1:
-        cidade_escolhida = input(nome, 'escolha a cidade')
+        cidade_escolhida = int(input('escolha a cidade'))
         cidade_certa = random.choice(cidades)
         if cidade_certa == cidades[cidade_escolhida-1]:
-            print(nome, 'Você venceu o jogo')
+            print(nome, 'Voce venceu o jogo')
             print('cidade sorteada:', cidade_certa)
         else:
             print(nome, 'Infelizmente você não venceu')
             print('cidade sorteada:', cidade_certa)
     else:
-        numero_escolhido = int(input(nome, 'escolha um número'))
-
+        numero_escolhido = int(input('escolha um número'))
+        numero_certo = random.choice(numeros)
+        if numero_escolhido == numero_certo:
+            print(nome, 'Você venceu o jogo')
+            print('numero  sorteado:', numero_certo)
+        else:
+            print(nome, 'Infelizmente você não venceu')
+            print('numero sorteado:', numero_certo)
     tentativas += 1
