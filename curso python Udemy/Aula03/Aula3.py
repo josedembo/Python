dabcb23c89f3372c6779c81e721c02b07ceffb12
José Pedro Daniel Dembo <68882941+josedembo@users.noreply.github.com>
2021-03-07 17:54:10 -0300

    aulas (de 02 á 05)

---
+++ curso python Udemy/Aula03/Aula3.py
@@ -0,0 +1,11 @@
+'''
+str = string
+'''
+
+print('Olá')
+print("Olá")
+print('''Olá''')
+print("Essa é uma 'String', (str)")
+print("Esse tecto é uma \"String\"")
+print("o meu numdo é uma \n String")
+print(r"o meu numdo é uma \n String") # o r faz com que nada seja executado dentro da fução print
---
+++ curso python Udemy/Aula04/aula04.py
@@ -0,0 +1,17 @@
+print(type('Jose'))
+print('Jose', type('Jose'))
+print(1234, type(1234))
+print(1.76, type(1.76))
+print(1.76 == 1, type(1.76 == 1))
+print('Luiz',type('Luiz'), bool('Luiz'))
+print('10', type('10'), type(int('10')))
+print(10.6, int(10.6))
+
+# String: nome
+print('José', type('José'))
+# int: idade
+print(21, type(21))
+# float: Altura
+print(1.80, type(1.80))
+# bool: maior de idade
+print(21>18, type(21>18))
