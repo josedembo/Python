b95ba191f37a6b12959dfa206a22ad32e906f668
José Pedro Daniel Dembo <68882941+josedembo@users.noreply.github.com>
2021-03-07 18:07:16 -0300

    Update Aula05.py

---
+++ curso python Udemy/Aula05/Aula05.py
@@ -0,0 +1,19 @@
+'''
+                Operadores artiméticos
++ adição
+- subtração
+* multiplicação
+/ divisão
+// divisão inteira
+** Exponenciação
+% resto
+() parentes:oredem de precedência
+'''
+
+print('Adição +', 10 + 10)  # adição
+print('multiplicação *', 10 * 10)  # multiplicação
+print('divisão /',10 / 10)  # divisão
+print('subtração',10 - 10)  # subtração
+print('divisão inteira //', 10 // 3)  # divisão inteira
+print('expoente **', 10 ** 2)  # exponenciação
+
