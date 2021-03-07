420d96ad2b1a063df37f64bec6afccd680ae0305
José Pedro Daniel Dembo <68882941+josedembo@users.noreply.github.com>
2021-03-07 16:45:54 -0300

    aulas do curso(aula 1 e 2)

---
+++ curso python Udemy/Aula01/Aula01.py
@@ -0,0 +1,15 @@
+# isso é um cometário
+print('linha1')
+print('linha2') # isso motra a linha 2 na tela
+# falando sobre ciência
+print('linha3')
+# print('linha4')
+print('linha5')  # isso é um comentário
+
+"""
+print('linha5')
+print('linha5')
+print('linha5')
+print('linha5')
+print('linha5')
+"""
