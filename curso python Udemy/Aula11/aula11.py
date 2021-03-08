046ebbfdea89d11373936dec95a01414c370fb0a
José Pedro Daniel Dembo <68882941+josedembo@users.noreply.github.com>
2021-03-08 07:21:16 -0300

    aulas

--- curso python Udemy/Aula11/aula11.py
+++ curso python Udemy/Aula11/aula11.py
@@ -1,3 +1,14 @@
 '''
 condicionais IF, ELIF, ELSE
 '''
+if False:
+    print('a')
+elif False:
+    print('Também é verdadeiro')
+elif True:
+    nome =input('Qual é o seu nome')
+    print(f'seu nome é {nome}')
+elif False:
+    print(22+18)
+else:
+    print('nada foi excutado')
