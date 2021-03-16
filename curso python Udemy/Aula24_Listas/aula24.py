af6a4bafa8cda4934df50ee91950efb85f72bc05
José Pedro Daniel Dembo <68882941+josedembo@users.noreply.github.com>
2021-03-15 23:31:35 -0300

    Update aula24.py

--- curso python Udemy/Aula24_Listas/aula24.py
+++ curso python Udemy/Aula24_Listas/aula24.py
@@ -5,5 +5,15 @@ append, insert, pop, del, clear, extend, +
 min, max
 '''
 lista = ['A','B','C']
+lista_2 = ['A', 'B', 'C', 'E', 10.7]
 nova_lista = lista[1:]
-print(nova_lista)
+print(lista_2[::-2])
+
+l1 = [4,3,6,8]
+l2 = [9,6,3]
+
+l3 = l1 + l2
+l1.extend(l2)
+l1.insert(0, 20)
+print(l3)
+print(l1)
