f084f7a783d73d0d95e2830f03a56104aa076841
José Pedro Daniel Dembo <68882941+josedembo@users.noreply.github.com>
2021-03-17 05:51:30 -0300

    update lesson 24 and a game

--- curso python Udemy/Aula24_Listas/aula24.py
+++ curso python Udemy/Aula24_Listas/aula24.py
@@ -9,11 +9,31 @@ lista_2 = ['A', 'B', 'C', 'E', 10.7]
 nova_lista = lista[1:]
 print(lista_2[::-2])
 
-l1 = [4,3,6,8]
-l2 = [9,6,3]
+l1 = [4, 3, 6, 8]
+l2 = [9, 6, 3]
 
 l3 = l1 + l2
 l1.extend(l2)
-l1.insert(0, 20)
-print(l3)
 print(l1)
+l1.insert(0, 20)
+l1.insert(0, 'Hello word')
+print(l1)
+l1.pop()
+print(l1)
+del(l1[0])
+print(l1)
+del(l1[2])
+print(l1)
+print(max(l1))
+print(min(l1))
+
+soma = 0
+for valor in l1:
+    soma = soma + valor
+
+print(f'soma = {soma}')
+print(f'soma do max e min = {max(l1) + min(l1)}')
+
+l = ['string', True, 6.8, 10]
+for item in l:
+    print(f'{item}', type(item))
---
+++ curso python Udemy/Aula24_Listas/jogo.py
@@ -0,0 +1,38 @@
+secriword = 'Wonderfull'
+digitadas = []
+
+chances = 4
+while True:
+    if chances == 0:
+        print(f'chances: {chances}')
+        print('VOCÊ PERDEU')
+        break
+    else:
+        print(f'chances: {chances}')
+    letra = input('digite uma letra: ')
+    if len(letra) > 1:
+        print('Hooow! não vale, digite apenas uma letra')
+        continue
+    digitadas.append(letra)
+
+    if letra in secriword:
+        print(f'MUITO BOM, EXISTE a letra "{letra}"  na palavra secreta')
+    else:
+        print(f'HOOWW, a letra "{letra}", NÃO EXISTE na palavra secreta')
+        digitadas.pop()
+        chances -= 1
+
+    n = len(secriword)
+    secreta_temp = ''
+
+    for letra_secreta in secriword:
+        if letra_secreta in digitadas:
+            secreta_temp += letra_secreta
+        else:
+            secreta_temp += '*'
+
+    if secreta_temp == secriword:
+        print(f'Que legal, você GANHOU, a palavra secreta é {secriword}')
+        break
+    else:
+        print(f'a palavra secreta está assim: {secreta_temp}')
